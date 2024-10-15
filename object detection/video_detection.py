62090c55b9d95bc3aa99f3a23df50ba13c919d7f
artemovmak <99392974+artemovmak@users.noreply.github.com>
2024-10-15 09:09:21 +0300

    faster detection

--- object detection/video_detection.py
+++ object detection/video_detection.py
@@ -13,7 +13,6 @@ colors = [
     (47, 79, 79), (47, 79, 47), (0, 206, 209), (148, 0, 211), (255, 20, 147)
 ]
 
-
 capture = cv2.VideoCapture("http://192.168.2.157:8081/")
 
 # Чтение параметров видео
@@ -21,10 +20,8 @@ fps = int(capture.get(cv2.CAP_PROP_FPS))
 width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# Настройка выходного файла
-# output_video_path = 'match.mp4'
-# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-# writer = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+
+frame_counter = 0  # Счетчик кадров
 
 while True:
     # Захват кадра
@@ -32,25 +29,33 @@ while True:
     if not ret:
         break
 
-    # Обработка кадра с помощью модели YOLO
-    results = model(frame)[0]
+    # Обрабатываем каждый второй кадр
+    if frame_counter % 2 == 0:
+        # Обработка кадра с помощью модели YOLO
+        results = model(frame)[0]
 
-    # Получение данных об объектах
-    classes_names = results.names
-    classes = results.boxes.cls.cpu().numpy()
-    boxes = results.boxes.xyxy.cpu().numpy().astype(np.int32)
-    # Рисование рамок и подписей на кадре
-    for class_id, box, conf in zip(classes, boxes, results.boxes.conf):
-        if conf>0.5:
-            class_name = classes_names[int(class_id)]
-            color = colors[int(class_id) % len(colors)]
-            x1, y1, x2, y2 = box
-            print(class_name, x1, y1, x2, y2)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-            cv2.putText(frame, class_name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        # Получение данных об объектах
+        classes_names = results.names
+        classes = results.boxes.cls.cpu().numpy()
+        boxes = results.boxes.xyxy.cpu().numpy().astype(np.int32)
 
-    # Вывод обработанного кадра на экран
-    cv2.imshow('Video', frame)
+        # Рисование рамок и подписей на кадре
+        for class_id, box, conf in zip(classes, boxes, results.boxes.conf):
+            if conf > 0.5:
+                class_name = classes_names[int(class_id)]
+                color = colors[int(class_id) % len(colors)]
+                x1, y1, x2, y2 = box
+                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                cv2.putText(frame, class_name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+    # Вывод обработанного кадра в окно (даже если он не был обработан моделью)
+    cv2.imshow('YOLOv8 Live', frame)
+
+    # Увеличиваем счетчик кадров
+    frame_counter += 1
+
+    # Выход по нажатию клавиши 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
